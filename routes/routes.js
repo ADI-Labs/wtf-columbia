d41af18111aca5f0e38bd31ded03ba2a5c8d636b
Nick Liu <nl2523@columbia.edu>
2016-02-26 16:26:38 -0500

    Add routing

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,15 @@
+
+var path = require('path');
+
 module.exports = function (app){
+	
+	
 	app.get('/', function (req, res) {
 		res.render('index', {title : "Hello World"})
-	})
+	});
+
+	app.get('/basicUI', function(req, res) {
+  		res.sendFile(path.join(__dirname, '../public/views/basicUI.html'))
+	});
+
 }
