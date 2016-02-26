7ba1f0a1ec3b2cbfe68107c42f95a818ad4e13c9
Nick Liu <nl2523@columbia.edu>
2016-02-26 17:37:03 -0500

    Add submit post + display posts

---
+++ public/static/textPost.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    console.log("test");
+    $("div#form1").append(
+        $("<h3/>").text("Submit Post"), $("<p/>").text("Fill out the form"), $("<form/>", {
+            
+        }).append(
+            $("<textarea/>", {
+                rows: '5px',
+                cols: '27px',
+                type: 'text',
+                id: 'vmsg',
+                name: 'msg',
+                placeholder: 'Message'
+            })/*, $("<br/>"), $("<input/>", {
+                type: 'submit',
+                id: 'submit',
+                value: 'Submit'
+            })*/
+        )
+    )
+
+    $("div#submit-btn").click(function() {
+        var msgContent = $("#vmsg").val();
+        displayData(msgContent);
+        console.log("test");
+    });
+
+
+});
+
+
+function displayData(msgContent1) {
+    $("ul#msg-data").append("<li style=\"list-style-type: none;\"> Content: " + msgContent1 + "</li>");
+}
