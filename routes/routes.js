7667ac6f85028e389ed90367c9f2f635f50dd10a
Howon <howonbyun@gmail.com>
2016-03-04 12:30:49 -0500

    referring back to jade

--- routes/routes.js
+++ routes/routes.js
@@ -2,14 +2,9 @@
 var path = require('path');
 
 module.exports = function (app){
-	
-	
+
+
 	app.get('/', function (req, res) {
-		res.render('index', {title : "Hello World"})
+		res.render('basicUI', {title : "Hello World"})
 	});
-
-	app.get('/basicUI', function(req, res) {
-  		res.sendFile(path.join(__dirname, '../public/views/basicUI.html'))
-	});
-
 }
